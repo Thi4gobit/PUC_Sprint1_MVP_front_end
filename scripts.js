a529bb765b27b47c6a22452f669658102ba8a3e2
Thiago Bittencourt <125612488+Thi4gobit@users.noreply.github.com>
2023-04-27 00:08:02 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,143 @@
+/*
+  --------------------------------------------------------------------------------------
+  Função para obter a lista existente do servidor via requisição GET
+  --------------------------------------------------------------------------------------
+*/
+const getList = async () => {
+    let url = 'http://127.0.0.1:5000/services';
+    fetch(url, {
+      method: 'get',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        data.services.forEach(item => insertList(item.code, item.baseDescription, item.unit))
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+  
+  /*
+    --------------------------------------------------------------------------------------
+    Chamada da função para carregamento inicial dos dados
+    --------------------------------------------------------------------------------------
+  */
+  getList()
+  
+  
+  /*
+    --------------------------------------------------------------------------------------
+    Função para colocar um item na lista do servidor via requisição POST
+    --------------------------------------------------------------------------------------
+  */
+  const postItem = async (inputCode, inputBaseDescription, inputUnit) => {
+    const formData = new FormData();
+    formData.append('code', inputCode);
+    formData.append('baseDescription', inputBaseDescription);
+    formData.append('unit', inputUnit);
+  
+    let url = 'http://127.0.0.1:5000/service';
+    fetch(url, {
+      method: 'post',
+      body: formData
+    })
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+  
+  
+  /*
+    --------------------------------------------------------------------------------------
+    Função para criar um botão close para cada item da lista
+    --------------------------------------------------------------------------------------
+  */
+  const insertButton = (parent) => {
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    parent.appendChild(span);
+  }
+  
+  
+  /*
+    --------------------------------------------------------------------------------------
+    Função para remover um item da lista de acordo com o click no botão close
+    --------------------------------------------------------------------------------------
+  */
+  const removeElement = () => {
+    let close = document.getElementsByClassName("close");
+    // var table = document.getElementById('myTable');
+    let i;
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function () {
+        let div = this.parentElement.parentElement;
+        const nomeItem = div.getElementsByTagName('td')[0].innerHTML
+        if (confirm("Você tem certeza?")) {
+          div.remove()
+          deleteItem(nomeItem)
+          alert("Removido!")
+        }
+      }
+    }
+  }
+  
+  /*
+    --------------------------------------------------------------------------------------
+    Função para deletar um item da lista do servidor via requisição DELETE
+    --------------------------------------------------------------------------------------
+  */
+  const deleteItem = (item) => {
+    console.log(item)
+    let url = 'http://127.0.0.1:5000/service?code=' + item;
+    fetch(url, {
+      method: 'delete'
+    })
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+  
+  /*
+    --------------------------------------------------------------------------------------
+    Função para adicionar um novo item com nome, quantidade e valor 
+    --------------------------------------------------------------------------------------
+  */
+  const newItem = () => {
+    let inputCode = document.getElementById("newCode").value;
+    let inputBaseDescription = document.getElementById("newBaseDescription").value;
+    let inputUnit = document.getElementById("newUnit").value;
+  
+    if (inputCode === '' || inputBaseDescription === '' || inputUnit === '') {
+      alert("Escreva o nome de um item!");
+    } else {
+      insertList(inputCode, inputBaseDescription, inputUnit)
+      postItem(inputCode, inputBaseDescription, inputUnit)
+      alert("Item adicionado!")
+    }
+  }
+  
+  /*
+    --------------------------------------------------------------------------------------
+    Função para inserir items na lista apresentada
+    --------------------------------------------------------------------------------------
+  */
+  const insertList = (code, baseDescription, unit) => {
+    var item = [code, baseDescription, unit]
+    var table = document.getElementById('myTable');
+    var row = table.insertRow();
+  
+    for (var i = 0; i < item.length; i++) {
+      var cel = row.insertCell(i);
+      cel.textContent = item[i];
+    }
+    insertButton(row.insertCell(-1))
+    document.getElementById("newCode").value = "";
+    document.getElementById("newBaseDescription").value = "";
+    document.getElementById("newUnit").value = "";
+  
+    removeElement()
+  }
